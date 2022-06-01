41044255c428f2d74e94afe776df1b6df2df615b
kocoten1992 <ko_co_ten_1992@yahoo.com>
2022-06-01 21:50:02 +0700

    feat: encode util add gzipFile method

--- src/EncodeUtil.php
+++ src/EncodeUtil.php
@@ -17,4 +17,42 @@ class EncodeUtil
     {
         return base64_decode(str_replace(['-','_'], ['+','/'], $string));
     }
+
+    public static function gzipFile(
+        string $source_path,
+        string|null $dest_path = null,
+        int $level = 6
+    ): string|false {
+        if (is_null($dest_path)) {
+            $dest_path = $source_path.'gz';
+        }
+
+        $mode = 'wb'.$level;
+
+        $error = false;
+
+        $fp_out = gzopen($dest_path, $mode);
+
+        if ($fp_out === false) {
+            return false;
+        }
+
+        $fp_in = fopen($source_path, 'rb');
+
+        if ($fp_in === false) {
+            return false;
+        }
+
+        while (! feof($fp_in)) {
+            gzwrite($fp_out, fread($fp_in, 1024 * 512));
+        }
+
+        $fclose = fclose($fp_in);
+
+        if ($fclose === false) {
+            return false;
+        }
+
+        return $dest_path;
+    }
 }
--- tests/EncodeUtilTest.php
+++ tests/EncodeUtilTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Factory;
 use PHPUnit\Framework\TestCase;
 use Talmp\Phputils\EncodeUtil;
 
@@ -18,5 +19,53 @@ class EncodeUtilTest extends TestCase
         $decoded_str = EncodeUtil::base64DecodeUrl($encoded_str);
 
         $this->assertEquals($string, $decoded_str);
+    }
+
+    public function test_gzip_file()
+    {
+        $faker = Faker\Factory::create();
+
+        $text = '';
+
+        $rand = rand(10, 40);
+
+        while ($rand) {
+            $rand -= 1;
+
+            $text .= $faker->paragraph()."\n";
+        }
+
+        $file_name = bin2hex(random_bytes(10));
+        $file_path = sys_get_temp_dir().'/'.$file_name;
+
+        $this->assertFalse(file_exists($file_path));
+
+        $touch = touch($file_path);
+
+        $this->assertTrue($touch);
+
+        $fpc = file_put_contents($file_path, $text);
+
+        $this->assertTrue($fpc !== false);
+
+        $dest_path = EncodeUtil::gzipFile($file_path);
+
+        $this->assertEquals($dest_path, $file_path.'gz');
+
+        $this->assertTrue(file_exists($dest_path));
+
+        $this->assertGreaterThan(filesize($dest_path), filesize($file_path));
+
+        // test custom dest_path
+        $custom_dest_path = sys_get_temp_dir().'/'.bin2hex(random_bytes(10));
+
+        $result_dest_path = EncodeUtil::gzipFile($file_path, $custom_dest_path);
+
+        $this->assertEquals($custom_dest_path, $result_dest_path);
+
+        // clean up
+        $this->assertTrue(unlink($file_path));
+        $this->assertTrue(unlink($dest_path));
+        $this->assertTrue(unlink($custom_dest_path));
     }
 }
